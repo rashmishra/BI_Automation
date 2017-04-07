9ad1f37fadea79490f35f1af669a2d608c89e899
Rashmi Mishra <rashmimishra@GODEL0495.local>
2017-04-07 18:11:45 +0530

    added month veriable in Daily_Web_Transactions and source table in refund

--- web_txn/Daily_Web_Transactions.sh
+++ web_txn/Daily_Web_Transactions.sh
@@ -1,15 +1,27 @@
 
-
 #!/bin/bash
 
 export TZ=Asia/Calcutta
 #get the month name for the folder creation
 
-MONTH_NAME=$(date +%b)
-echo $MONTH_NAME
+# v_month_name=`date +%b`;
+v_month_name=`date -d " -1 day" +%b`;
+v_month_number=`date +%m`;
+v_querying_month=`date -d " -1 day" +%m`;
+v_manager_name=$1;
 
-echo $1
-echo $2
+echo "${v_manager_name} it the Daily_Web_Transactions";
+echo "${v_month_name} is the Month Name, used in Folder's name";
+echo "${v_month_number} is the Month Number, used in the query";
+
+
+if [ ! -d "/home/ubuntu/BI/data/google_drive/Daily_Web_Transactions/${v_month_name}/" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir /home/ubuntu/BI/data/google_drive/Daily_Web_Transactions/${v_month_name}/ ;
+  chmod 0777 /home/ubuntu/BI/data/google_drive/Daily_Web_Transactions/${v_month_name} ;
+else echo "Directory for the month exists.";
+fi
+
 
 bq --format=csv query --n=10000000 "SELECT
   oh.orderid AS orderid,
@@ -56,7 +68,8 @@ GROUP BY
   source,
   t.transactiontype
   
-order by createdat desc
-"> /home/ubuntu/BI/data/google_drive/Daily_Web_Transactions/Apr/$1.csv
+order by createdat desc" > /home/ubuntu/BI/data/google_drive/Daily_Web_Transactions/${v_month_name}/${v_manager_name}.csv
+
 
 exit 0
+
